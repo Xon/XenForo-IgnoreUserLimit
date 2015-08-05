b3e1b459acdd1f11ac1d155b8979dec811e861e4
Xon <tazipper1@hotmail.com>
2015-08-05 08:54:31 +0800

    Trim whitespace

--- upload/library/SV/IgnoreUserLimit/Listener.php
+++ upload/library/SV/IgnoreUserLimit/Listener.php
@@ -13,8 +13,8 @@ class SV_IgnoreUserLimit_Listener
         if ($version == 0)
         {
             // make sure XenForo_Model_User is extended
-            XenForo_Model::create("XenForo_Model_User");            
-            
+            XenForo_Model::create("XenForo_Model_User");
+
             $db->query("insert ignore into xf_permission_entry (user_group_id, user_id, permission_group_id, permission_id, permission_value, permission_value_int)
                 select " . XenForo_Model_User::$defaultRegisteredGroupId . ", 0, 'general', 'sv_userIgnoreLimit', 'allow', '-1'
                 from xf_permission_entry
