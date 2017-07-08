fe2143f9598d20cd179201b0a7cf0b30da68d785
Xon <tazipper1@hotmail.com>
2017-07-08 17:47:09 +0800

    Improve error reporting of add-on installer & require php 5.4

--- upload/library/SV/IgnoreUserLimit/Installer.php
+++ upload/library/SV/IgnoreUserLimit/Installer.php
@@ -5,9 +5,15 @@ class SV_IgnoreUserLimit_Installer
     public static function install($installedAddon, array $addonData, SimpleXMLElement $xml)
     {
         $version = isset($installedAddon['version_id']) ? $installedAddon['version_id'] : 0;
+        $required = '5.4.0';
+        $phpversion = phpversion();
+        if (version_compare($phpversion, $required, '<'))
+        {
+            throw new XenForo_Exception("PHP {$required} or newer is required. {$phpversion} does not meet this requirement. Please ask your host to upgrade PHP", true);
+        }
         if (XenForo_Application::$versionId < 1030070)
         {
-            throw new Exception('XenForo 1.3.0+ is Required!');
+            throw new XenForo_Exception('XenForo 1.3.0+ is Required!', true); // Make this show nicely.
         }
 
         $db = XenForo_Application::getDb();
